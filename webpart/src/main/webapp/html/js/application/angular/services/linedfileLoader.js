4ae02fc26af4b4629c81f96f4af68f97e20120d0
d.tolmachov <dmitolm@gmail.com>
2015-02-07 02:45:06 +0200

    - methods from mdxLoader moved to linedfileLoader

--- webpart/src/main/webapp/html/js/application/angular/services/linedfileLoader.js
+++ webpart/src/main/webapp/html/js/application/angular/services/linedfileLoader.js
@@ -18,6 +18,112 @@
                 function LinedFileObj(){
                     this.loadDataAtOffset = function (offset, length) {
                         return fileReadHelper(a, offset, length)
+                    };
+                    this.readType = function(fileObject, sectionDef, offset, len) {
+                        var result;
+
+                        var type = sectionDef.type;
+                        switch (type) {
+                            case "int16":
+                                result = fileObject.readInt16(offset);
+                                break;
+                            case "int32" :
+                                result = fileObject.readInt32(offset);
+                                break;
+                            case "uint16":
+                                result = fileObject.readUint16(offset);
+                                break;
+                            case "uint32":
+                                result = fileObject.readUint32(offset);
+                                break;
+                            case "uint8Array" :
+                                result = fileObject.readUint8Array(offset, len);
+                                break;
+                            case "uint16Array" :
+                                result = fileObject.readUint16Array(offset, len);
+                                break;
+                            case "vector3f" :
+                                result = fileObject.readVector3f(offset);
+                                break;
+                            case "float32" :
+                                result = fileObject.readFloat32(offset);
+                                break;
+                            case "string" :
+                                if (len != undefined) {
+                                    result = fileObject.readNZTString(offset, len);
+                                } else {
+                                    result = fileObject.readString(offset, 9999);
+                                }
+                                break;
+                            case "layout" :
+                                /*
+                                 * Parse layout
+                                 */
+                                var layout = sectionDef.layout;
+                                var resultObj = {};
+
+                                if (!layout instanceof Array) {
+                                    throw "layout is not array";
+                                }
+
+                                for (var i = 0; i < layout.length; i++) {
+                                    var paramName = layout[i].name;
+                                    resultObj[paramName] = this.parseSectionDefinition(resultObj, layout[i], fileObject, offset);
+                                }
+
+                                return resultObj;
+
+                            default:
+                                $log.info("Unknown type in layout. type = ", type);
+                                break
+                        }
+
+                        return result;
+                    }
+
+                    this.parseSectionDefinition = function(parentObject, sectionDefinition, fileObject, offset){
+                        var offs;
+                        if (typeof sectionDefinition.offset == "string") {
+                            offs = parentObject[sectionDefinition.offset];
+                        } else {
+                            //Assume this is number
+                            offs = sectionDefinition.offset;
+                        }
+
+                        var len = sectionDefinition.len;
+                        if (typeof sectionDefinition.len == "string") {
+                            len = parentObject[len];
+                        }
+
+                        if (offset && offs) {
+                            offset =  {offs : offs};
+                        } else if (offs){
+                            offset = { offs : offs };
+                        } else if (!offset){
+                            offset = { offs : 0};
+                        }
+
+                        var fieldObject;
+                        var fieldArray = [];
+
+                        var count = 1;
+                        var countIsGiven = sectionDefinition.count !== undefined;
+                        if (countIsGiven) {
+                            count = parentObject[sectionDefinition.count];
+                        }
+
+                        for (var j = 0; j < count; j++) {
+                            fieldObject = this.readType(fileObject, sectionDefinition, offset, len);
+                            fieldArray.push(fieldObject);
+                        }
+                        var resultObj;
+                        if (countIsGiven) {
+                            resultObj = fieldArray;
+                        } else {
+                            resultObj = fieldArray[0];
+                        }
+
+                        return resultObj;
                     }
                 }
                 LinedFileObj.prototype = fileReader;
--- webpart/src/main/webapp/html/js/application/angular/services/map/mdxLoader.js
+++ webpart/src/main/webapp/html/js/application/angular/services/map/mdxLoader.js
@@ -144,110 +144,6 @@
         var mdxTablePerVersion = {
             "264" : mdx_ver264
         };
-        function readType(fileObject, sectionDef, offset, len) {
-            var result;
-
-            var type = sectionDef.type;
-            switch (type) {
-                case "int32" :
-                    result = fileObject.readInt32(offset);
-                    break;
-                case "uint16":
-                    result = fileObject.readUint16(offset);
-                    break;
-                case "uint32":
-                    result = fileObject.readUint32(offset);
-                    break;
-                case "uint8Array" :
-                    result = fileObject.readUint8Array(offset, len);
-                    break;
-                case "vector3f" :
-                    result = fileObject.readVector3f(offset);
-                    break;
-                case "float32" :
-                    result = fileObject.readFloat32(offset);
-                    break;
-                case "string" :
-                    if (len != undefined) {
-                        result = fileObject.readNZTString(offset, len);
-                    } else {
-                        result = fileObject.readString(offset, 9999);
-                    }
-                    break;
-                case "layout" :
-                    /*
-                    * Parse layout
-                    */
-                    var layout = sectionDef.layout;
-                    var resultObj = {};
-
-                    if (!layout instanceof Array) {
-                        throw "layout is not array";
-                    }
-
-                    for (var i = 0; i < layout.length; i++) {
-                        var paramName = layout[i].name;
-                        resultObj[paramName] = parseSectionDefinition(resultObj, layout[i], fileObject, offset);
-                    }
-
-                    return resultObj;
-
-                default:
-                    $log.info("Unknown type in layout. type = ", type);
-                    break
-            }
-
-            return result;
-        }
-
-        function parseSectionDefinition(parentObject, sectionDefinition, fileObject, offset){
-            var offs;
-            if (typeof sectionDefinition.offset == "string") {
-                offs = parentObject[sectionDefinition.offset];
-            } else {
-                //Assume this is number
-                offs = sectionDefinition.offset;
-            }
-
-            var len = sectionDefinition.len;
-            if (typeof sectionDefinition.len == "string") {
-                len = sectionDefinition[len];
-            }
-
-            if (offset && offs) {
-                offset =  {offs : offs};
-            } else if (offs){
-                offset = { offs : offs };
-            } else if (!offset){
-                offset = { offs : 0};
-            }
-
-
-            var layout = sectionDefinition.layout;
-
-            var fieldObject;
-            var fieldArray = [];
-
-            var count = 1;
-            var countIsGiven = sectionDefinition.count !== undefined;
-            if (countIsGiven) {
-                count = parentObject[sectionDefinition.count];
-            }
-
-            for (var j = 0; j < count; j++) {
-                fieldObject = readType(fileObject, sectionDefinition, offset, len);
-                fieldArray.push(fieldObject);
-            }
-            var resultObj;
-            if (countIsGiven) {
-                resultObj = fieldArray;
-            } else {
-                resultObj = fieldArray[0];
-            }
-
-            return resultObj;
-        }
-
 
         return function(filePath) {
             var deferred = $q.defer();
@@ -277,7 +173,7 @@
                     header : {}
                 };
 
-                resultMDXObject.header = parseSectionDefinition(resultMDXObject, mdxDescription, fileObject, offset);
+                resultMDXObject.header = fileObject.parseSectionDefinition(resultMDXObject, mdxDescription, fileObject, offset);
 
                 deferred.resolve(resultMDXObject);
             },
