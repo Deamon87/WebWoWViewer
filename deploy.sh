f9c36598eea0774449fb20fc4e1c9f7a3daccdb7
Deamon87 <dmitolm@gmail.com>
2016-09-28 16:54:22 +0300

    fixes2

--- deploy.sh
+++ deploy.sh
@@ -34,12 +34,13 @@ rm -rf dist/**/* || exit 0
 doCompile
 
 # Now let's go have some fun with the cloned repo
+
 cd dist
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z "git diff --exit-code"]; then
+if [ -z "git diff --exit-code" ]; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
@@ -54,7 +55,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ./../deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
