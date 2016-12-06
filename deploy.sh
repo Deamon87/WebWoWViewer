e12173980f85d79b5c3167de5796f6fbf5022ecc
Deamon87 <dmitolm@gmail.com>
2016-12-06 19:18:36 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,6 @@ set -e # Exit with nonzero exit code if anything fails
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
-function doCompile {
-
-}
-
 #$1 - branch
 #$2 - folder
 function compileBuild {
@@ -23,7 +19,6 @@ function compileBuild {
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
-    doCompile
     exit 0
 fi
 
